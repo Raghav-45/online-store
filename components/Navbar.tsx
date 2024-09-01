9caa956126af7c4e2c7a436c718699725dc9a84f
Aditya <raghavbhai4545@gmail.com>
2024-09-01 22:05:31 +0530

    Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,52 @@
+import { FC } from 'react'
+import { Button } from './ui/button'
+import {
+  AlignJustifyIcon,
+  FootprintsIcon,
+  ShoppingCartIcon,
+} from 'lucide-react'
+import Link from 'next/link'
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = ({}) => {
+  return (
+    <nav className="relative flex items-center justify-between p-4 lg:px-6">
+      <div className="block flex-none md:hidden">
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-11 w-11 md:hidden bg-neutral-900 border-neutral-700 text-white"
+        >
+          <AlignJustifyIcon className="h-3.5 w-3.5" />
+        </Button>
+      </div>
+      <div className="flex w-full items-center">
+        <div className="flex w-full md:w-1/3">
+          <Link
+            href="/"
+            className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
+          >
+            <div className="flex flex-none items-center justify-center border border-neutral-700 bg-black h-[40px] w-[40px] rounded-xl">
+              <FootprintsIcon className="h-4 w-4" />
+            </div>
+            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
+              Online Store
+            </div>
+          </Link>
+        </div>
+        <div className="flex justify-end md:w-1/3">
+          <Button
+            variant="outline"
+            size="icon"
+            className="h-11 w-11 md:hidden bg-neutral-900 border-neutral-700 text-white"
+          >
+            <ShoppingCartIcon className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
