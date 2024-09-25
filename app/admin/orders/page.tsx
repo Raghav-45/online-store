fc14e8133ba96a1fbfd41efc95811c6e402a0827
Aditya <raghavbhai4545@gmail.com>
2024-09-25 13:06:27 +0530

    format

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -14,14 +14,9 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer"
+} from '@/components/ui/drawer'
 
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 import {
   Card,
@@ -30,7 +25,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from '@/components/ui/card'
 
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
@@ -41,9 +36,9 @@ import { Textarea } from '@/components/ui/textarea'
 import { PlusIcon } from 'lucide-react'
 import DetailsForm from '@/components/DetailsForm'
 
-interface OrderPageProps { }
+interface OrderPageProps {}
 
-const OrderPage: FC<OrderPageProps> = ({ }) => {
+const OrderPage: FC<OrderPageProps> = ({}) => {
   const [allOrders, setAllOrders] = useState<OrderType[]>()
   const [selectedOrder, setSelectedOrder] = useState<OrderType>()
 
@@ -73,29 +68,40 @@ const OrderPage: FC<OrderPageProps> = ({ }) => {
 
           <Tabs defaultValue="new-order" className="w-full">
             <TabsList className="grid w-full grid-cols-4">
-              <TabsTrigger className="text-xs" value="new-order">New Order</TabsTrigger>
-              <TabsTrigger className="text-xs" value="shipping">Shipping</TabsTrigger>
-              <TabsTrigger className="text-xs" value="completed">Completed</TabsTrigger>
-              <TabsTrigger className="text-xs" value="cancelled">Cancelled</TabsTrigger>
+              <TabsTrigger className="text-xs" value="new-order">
+                New Order
+              </TabsTrigger>
+              <TabsTrigger className="text-xs" value="shipping">
+                Shipping
+              </TabsTrigger>
+              <TabsTrigger className="text-xs" value="completed">
+                Completed
+              </TabsTrigger>
+              <TabsTrigger className="text-xs" value="cancelled">
+                Cancelled
+              </TabsTrigger>
             </TabsList>
             <TabsContent value="new-order">
               <div className="w-full h-auto">
                 <Drawer>
-                  {allOrders && allOrders.map((e) => (
-                    <DrawerTrigger key={e.paymentId} asChild>
-                      <div
-                        onClick={() => setSelectedOrder(e)}
-                        className="w-full rounded-lg h-10 bg-neutral-800 items-center flex justify-between px-4 mb-2"
-                      >
-                        <p className="text-xs">{e.paymentId}</p>
-                        <p className="text-xs">{e.price} INR</p>
-                      </div>
-                    </DrawerTrigger>
-                  ))}
+                  {allOrders &&
+                    allOrders.map((e) => (
+                      <DrawerTrigger key={e.paymentId} asChild>
+                        <div
+                          onClick={() => setSelectedOrder(e)}
+                          className="w-full rounded-lg h-10 bg-neutral-800 items-center flex justify-between px-4 mb-2"
+                        >
+                          <p className="text-xs">{e.paymentId}</p>
+                          <p className="text-xs">{e.price} INR</p>
+                        </div>
+                      </DrawerTrigger>
+                    ))}
                   <DrawerContent>
                     <DrawerHeader>
-                      <div className='flex justify-between pb-2 items-center'>
-                        <h3 className='text-xl font-semibold leading-none tracking-tight pl-0.5'>Order Details</h3>
+                      <div className="flex justify-between pb-2 items-center">
+                        <h3 className="text-xl font-semibold leading-none tracking-tight pl-0.5">
+                          Order Details
+                        </h3>
                         {/* <h3>{selectedOrder?.price ?? "X"} INR</h3> */}
                         <DrawerClose>
                           <Button
@@ -108,49 +114,66 @@ const OrderPage: FC<OrderPageProps> = ({ }) => {
                         </DrawerClose>
                       </div>
 
-                      <div className='flex gap-x-4 pb-4'>
-                        <div className='border w-1/3 aspect-square rounded-2xl'>
-                          <img src="https://cdn.shopify.com/s/files/1/0754/3727/7491/files/t-shirt-1.png" alt="Product Image" />
+                      <div className="flex gap-x-4 pb-4">
+                        <div className="border w-1/3 aspect-square rounded-2xl">
+                          <img
+                            src="https://cdn.shopify.com/s/files/1/0754/3727/7491/files/t-shirt-1.png"
+                            alt="Product Image"
+                          />
                         </div>
-                        <div className='flex flex-col w-2/3 text-left py-2.5 gap-y-1.5'>
-                          <div className='w-full pr-2'>
-                            <h3 className='text-lg font-extrabold line-clamp-2 leading-none tracking-tight text-ellipsis overflow-hidden'>Mens Sportswear Shoes</h3>
+                        <div className="flex flex-col w-2/3 text-left py-2.5 gap-y-1.5">
+                          <div className="w-full pr-2">
+                            <h3 className="text-lg font-extrabold line-clamp-2 leading-none tracking-tight text-ellipsis overflow-hidden">
+                              Mens Sportswear Shoes
+                            </h3>
                           </div>
-                          <p className='text-md leading-none tracking-tight font-semibold'>Colour: green</p>
-                          <p className='text-md leading-none tracking-tight font-semibold'>Size: 12</p>
-                          <p className='text-md leading-none tracking-tight font-semibold'>Qty: 1</p>
+                          <p className="text-md leading-none tracking-tight font-semibold">
+                            Colour: green
+                          </p>
+                          <p className="text-md leading-none tracking-tight font-semibold">
+                            Size: 12
+                          </p>
+                          <p className="text-md leading-none tracking-tight font-semibold">
+                            Qty: 1
+                          </p>
                         </div>
                       </div>
 
-                      <div className='flex flex-col gap-y-4 text-left'>                        
+                      <div className="flex flex-col gap-y-4 text-left">
                         <div className="grid w-full items-center gap-1.5">
-                          <Label className="ml-1" htmlFor="email">Shipping Address</Label>
+                          <Label className="ml-1" htmlFor="email">
+                            Shipping Address
+                          </Label>
                           <Textarea
                             value="A-105, Shivaji Vihar, Rajouri Garden, Rajouri Garden, Rajouri Garden, Rajouri Garden, New Delhi 110027"
-                            className='resize-none'
+                            className="resize-none"
                             rows={3}
                             disabled
                           />
                         </div>
 
-                        <div className='flex gap-x-2'>
+                        <div className="flex gap-x-2">
                           <div className="grid w-full items-center gap-1.5">
-                            <Label className="ml-1" htmlFor="email">Primary Contact</Label>
+                            <Label className="ml-1" htmlFor="email">
+                              Primary Contact
+                            </Label>
                             {/* <InputWithCopyButton text={"9315988300"} /> */}
                             <Input
-                              value={"9315988300"}
+                              value={'9315988300'}
                               type="text"
-                              placeholder={"9315988300"}
+                              placeholder={'9315988300'}
                               disabled
                             />
                           </div>
                           <div className="grid w-full items-center gap-1.5">
-                            <Label className="ml-1" htmlFor="email">Alternate Contact</Label>
+                            <Label className="ml-1" htmlFor="email">
+                              Alternate Contact
+                            </Label>
                             {/* <InputWithCopyButton text={"1234567890"} /> */}
                             <Input
-                              value={"1234567890"}
+                              value={'1234567890'}
                               type="text"
-                              placeholder={"1234567890"}
+                              placeholder={'1234567890'}
                               disabled
                             />
                           </div>
@@ -162,7 +185,9 @@ const OrderPage: FC<OrderPageProps> = ({ }) => {
                         </div> */}
 
                         <div className="grid w-full items-center gap-1.5">
-                          <Label className="ml-1" htmlFor="email">Order Id</Label>
+                          <Label className="ml-1" htmlFor="email">
+                            Order Id
+                          </Label>
                           <InputWithCopyButton text={selectedOrder?.orderId} />
                         </div>
 
@@ -173,12 +198,16 @@ const OrderPage: FC<OrderPageProps> = ({ }) => {
                       </div>
                     </DrawerHeader>
                     <DrawerFooter>
-                      <div className='flex gap-x-2'>
-                        <Button className='w-1/2 bg-red-500' variant="outline">Cancel</Button>
+                      <div className="flex gap-x-2">
+                        <Button className="w-1/2 bg-red-500" variant="outline">
+                          Cancel
+                        </Button>
                         {/* <Link className='w-full' href={`/product/${selectedOrder?.productId}`}>
                           <Button className='w-full'>Visit Product Page</Button>
                         </Link> */}
-                        <Button className='w-1/2 bg-blue-500' variant="outline">Ship</Button>
+                        <Button className="w-1/2 bg-blue-500" variant="outline">
+                          Ship
+                        </Button>
                       </div>
                     </DrawerFooter>
                   </DrawerContent>
@@ -188,44 +217,56 @@ const OrderPage: FC<OrderPageProps> = ({ }) => {
             <TabsContent value="shipping">
               <div className="w-full h-auto">
                 <Drawer>
-                  {allOrders && allOrders.slice(1, 4).map((e) => (
-                    <DrawerTrigger key={e.paymentId} asChild>
-                      <div
-                        onClick={() => setSelectedOrder(e)}
-                        className="w-full rounded-lg h-10 bg-neutral-800 items-center flex justify-between px-4 mb-2"
-                      >
-                        <p className="text-xs">{e.paymentId}</p>
-                        <p className="text-xs">{e.price} INR</p>
-                      </div>
-                    </DrawerTrigger>
-                  ))}
+                  {allOrders &&
+                    allOrders.slice(1, 4).map((e) => (
+                      <DrawerTrigger key={e.paymentId} asChild>
+                        <div
+                          onClick={() => setSelectedOrder(e)}
+                          className="w-full rounded-lg h-10 bg-neutral-800 items-center flex justify-between px-4 mb-2"
+                        >
+                          <p className="text-xs">{e.paymentId}</p>
+                          <p className="text-xs">{e.price} INR</p>
+                        </div>
+                      </DrawerTrigger>
+                    ))}
                   <DrawerContent>
                     <DrawerHeader>
-                      <div className='flex justify-between pb-4'>
-                        <h3 className='text-lg font-semibold leading-none tracking-tight'>{selectedOrder?.paymentId ?? "NA"}</h3>
-                        <h3>{selectedOrder?.price ?? "X"} INR</h3>
+                      <div className="flex justify-between pb-4">
+                        <h3 className="text-lg font-semibold leading-none tracking-tight">
+                          {selectedOrder?.paymentId ?? 'NA'}
+                        </h3>
+                        <h3>{selectedOrder?.price ?? 'X'} INR</h3>
                       </div>
-                      <div className='flex flex-col gap-y-4 text-left'>
-
+                      <div className="flex flex-col gap-y-4 text-left">
                         <div className="grid w-full items-center gap-1.5">
-                          <Label className="ml-1" htmlFor="email">Payment Id</Label>
-                          <InputWithCopyButton text={selectedOrder?.paymentId} />
+                          <Label className="ml-1" htmlFor="email">
+                            Payment Id
+                          </Label>
+                          <InputWithCopyButton
+                            text={selectedOrder?.paymentId}
+                          />
                         </div>
 
                         <div className="grid w-full items-center gap-1.5">
-                          <Label className="ml-1" htmlFor="email">Order Id</Label>
+                          <Label className="ml-1" htmlFor="email">
+                            Order Id
+                          </Label>
                           <InputWithCopyButton text={selectedOrder?.orderId} />
                         </div>
 
                         <div className="grid w-full items-center gap-1.5">
-                          <Label className="ml-1" htmlFor="email">Product Id</Label>
-                          <InputWithCopyButton text={selectedOrder?.productId} />
+                          <Label className="ml-1" htmlFor="email">
+                            Product Id
+                          </Label>
+                          <InputWithCopyButton
+                            text={selectedOrder?.productId}
+                          />
                         </div>
                       </div>
                     </DrawerHeader>
                     <DrawerFooter>
                       {/* <Link href={`/product/${selectedOrder?.productId}`}> */}
-                        <Button className='w-full'>Delivered</Button>
+                      <Button className="w-full">Delivered</Button>
                       {/* </Link> */}
                     </DrawerFooter>
                   </DrawerContent>
