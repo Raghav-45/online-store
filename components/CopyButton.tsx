5d2c4509d112f7b44f897bb0036a6f1d9a6caeb0
Aditya <raghavbhai4545@gmail.com>
2024-09-26 16:37:43 +0530

    Update CopyButton.tsx

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -1,25 +1,36 @@
-import { useState } from 'react';
-import copy from 'clipboard-copy';
-import { ClipboardIcon, CopyIcon } from 'lucide-react';
-import { Button } from './ui/button';
+import { useState } from 'react'
+import copy from 'clipboard-copy'
+import { ClipboardIcon, CopyIcon } from 'lucide-react'
+import { Button } from './ui/button'
 
 const CopyToClipboardButton = ({ text }: { text: string }) => {
-  const [isCopied, setIsCopied] = useState(false);
+  const [isCopied, setIsCopied] = useState(false)
 
   const handleCopyClick = async () => {
     try {
-      await copy(text);
-      setIsCopied(true);
+      await copy(text)
+      setIsCopied(true)
     } catch (error) {
-      console.error('Failed to copy text to clipboard', error);
+      console.error('Failed to copy text to clipboard', error)
     }
-  };
+  }
 
   return (
-    <Button variant="outline" size="icon" onClick={handleCopyClick}>
-      {isCopied ? <ClipboardIcon className="h-4 w-4" /> : <CopyIcon className="h-4 w-4" />}
-    </Button>
-  );
-};
+    <div>
+      <Button
+        // className="flex-none"
+        variant="outline"
+        size="icon"
+        onClick={handleCopyClick}
+      >
+        {isCopied ? (
+          <ClipboardIcon className="h-4 w-4" />
+        ) : (
+          <CopyIcon className="h-4 w-4" />
+        )}
+      </Button>
+    </div>
+  )
+}
 
 export default CopyToClipboardButton
