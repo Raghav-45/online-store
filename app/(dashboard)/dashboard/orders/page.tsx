c5de9cc0302b6edd518b0282cf6aea8d91b2ebce
Aditya <raghavbhai4545@gmail.com>
2024-10-03 16:36:48 +0530

    Update page.tsx

--- app/(dashboard)/dashboard/orders/page.tsx
+++ app/(dashboard)/dashboard/orders/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   ChevronLeft,
   ChevronRight,
@@ -9,7 +11,7 @@ import {
   Truck,
 } from 'lucide-react'
 
-import { format, formatDistance, subDays } from 'date-fns'
+import { format, formatDistance } from 'date-fns'
 
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
@@ -47,51 +49,31 @@ import {
 } from '@/components/ui/table'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { getAllOrderHistory } from '@/lib/dbUtils'
+import { useEffect, useState } from 'react'
 
-const description =
-  'An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information.'
+const currentDate = new Date()
 
-// const orders = [
-//   {
-//     customer: 'Liam Johnson',
-//     email: 'liam@example.com',
-//     type: 'Sale',
-//     status: 'Fulfilled',
-//     date: '2023-06-23',
-//     amount: '250.00',
-//     badgeVariant: 'secondary',
-//   },
-//   {
-//     customer: 'Olivia Smith',
-//     email: 'olivia@example.com',
-//     type: 'Refund',
-//     status: 'Declined',
-//     date: '2023-06-24',
-//     amount: '150.00',
-//     badgeVariant: 'outline',
-//   },
-//   {
-//     customer: 'Noah Williams',
-//     email: 'noah@example.com',
-//     type: 'Subscription',
-//     status: 'Fulfilled',
-//     date: '2023-06-25',
-//     amount: '350.00',
-//     badgeVariant: 'secondary',
-//   },
-//   {
-//     customer: 'Emma Brown',
-//     email: 'emma@example.com',
-//     type: 'Sale',
-//     status: 'Fulfilled',
-//     date: '2023-06-26',
-//     amount: '450.00',
-//     badgeVariant: 'secondary',
-//   },
-// ]
+export default function Orders() {
+  const [allOrders, setAllOrders] = useState<OrderType[]>()
+  const [selectedOrder, setSelectedOrder] = useState<OrderType>()
 
-export default async function Orders() {
-  let orders = await getAllOrderHistory()
+  const getAllOrders = async () => {
+    const data = await getAllOrderHistory()
+    return data
+  }
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      if (!allOrders) {
+        const orders = await getAllOrders()
+        setAllOrders(orders)
+        setSelectedOrder(orders[0]) // Set the first order after allOrders is updated
+      }
+    }
+
+    fetchOrders()
+  }, [])
+
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
@@ -185,9 +167,9 @@ export default async function Orders() {
                   <TableHeader>
                     <TableRow>
                       <TableHead>Customer</TableHead>
-                      <TableHead className="hidden sm:table-cell">
+                      {/* <TableHead className="hidden sm:table-cell">
                         Type
-                      </TableHead>
+                      </TableHead> */}
                       <TableHead className="hidden sm:table-cell">
                         Status
                       </TableHead>
@@ -198,36 +180,45 @@ export default async function Orders() {
                     </TableRow>
                   </TableHeader>
                   <TableBody>
-                    {orders.map((order, index) => (
-                      <TableRow key={index}>
-                        <TableCell>
-                          <div className="font-medium">
-                            {order.shippingAddress.fullName}
-                          </div>
-                          <div className="hidden text-sm text-muted-foreground md:inline">
-                            {order.shippingAddress.Contact}
-                          </div>
-                        </TableCell>
-                        <TableCell className="hidden sm:table-cell">
+                    {allOrders &&
+                      allOrders.map((order, index) => (
+                        <TableRow
+                          key={index}
+                          onClick={() => setSelectedOrder(order)}
+                          className={
+                            selectedOrder?.orderId == order.orderId
+                              ? 'bg-muted/50'
+                              : ''
+                          }
+                        >
+                          <TableCell>
+                            <div className="font-medium">
+                              {order.shippingAddress.fullName}
+                            </div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              {order.shippingAddress.Contact}
+                            </div>
+                          </TableCell>
+                          {/* <TableCell className="hidden sm:table-cell">
                           Sale
-                        </TableCell>
-                        <TableCell className="hidden sm:table-cell">
-                          <Badge className="text-xs" variant="outline">
-                            {order.status}
-                          </Badge>
-                        </TableCell>
-                        <TableCell className="hidden md:table-cell">
-                          {formatDistance(
-                            order.orderDate.toDate(),
-                            new Date(),
-                            { addSuffix: true }
-                          )}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          ₹{order.price}
-                        </TableCell>
-                      </TableRow>
-                    ))}
+                        </TableCell> */}
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="outline">
+                              {order.status}
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            {formatDistance(
+                              order.orderDate.toDate(),
+                              currentDate,
+                              { addSuffix: true }
+                            )}
+                          </TableCell>
+                          <TableCell className="text-right">
+                            ₹{order.price}
+                          </TableCell>
+                        </TableRow>
+                      ))}
                   </TableBody>
                 </Table>
               </CardContent>
@@ -240,7 +231,7 @@ export default async function Orders() {
           <CardHeader className="flex flex-row items-start bg-muted/50">
             <div className="grid gap-0.5">
               <CardTitle className="group flex items-center gap-2 text-lg">
-                Order Oe31b70H
+                Order {selectedOrder?.orderId ?? 'Oe31b70H'}
                 <Button
                   size="icon"
                   variant="outline"
@@ -250,7 +241,12 @@ export default async function Orders() {
                   <span className="sr-only">Copy Order ID</span>
                 </Button>
               </CardTitle>
-              <CardDescription>Date: November 23, 2023</CardDescription>
+              <CardDescription>
+                Date:{' '}
+                {selectedOrder
+                  ? format(selectedOrder?.orderDate.toDate(), 'PPP')
+                  : 'November 23, 2023'}
+              </CardDescription>
             </div>
             <div className="ml-auto flex items-center gap-1">
               <Button size="sm" variant="outline" className="h-8 gap-1">
@@ -277,38 +273,68 @@ export default async function Orders() {
           </CardHeader>
           <CardContent className="p-6 text-sm">
             <div className="grid gap-3">
+              {/* <div className="font-semibold">Order Details</div> */}
+
+              <div className="flex gap-x-4 pb-4">
+                <div className="border w-1/4 aspect-square rounded-2xl">
+                  <img
+                    src="https://cdn.shopify.com/s/files/1/0754/3727/7491/files/t-shirt-1.png"
+                    alt="Product Image"
+                  />
+                </div>
+                <div className="flex flex-col w-3/4 text-left py-2.5 gap-y-1.5">
+                  <div className="w-full pr-2">
+                    <h3 className="text-lg font-extrabold line-clamp-2 leading-none tracking-tight text-ellipsis overflow-hidden">
+                      {selectedOrder?.productName} x <span>1</span>
+                    </h3>
+                  </div>
+                  <ul className="grid gap-0">
+                    <li className="flex items-center justify-between text-muted-foreground">
+                      Colour: green
+                    </li>
+                    <li className="flex items-center justify-between text-muted-foreground">
+                      Size: 12
+                    </li>
+                    <li className="flex items-center justify-between text-muted-foreground">
+                      Qty: 1
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
               <div className="font-semibold">Order Details</div>
+
               <ul className="grid gap-3">
                 <li className="flex items-center justify-between">
                   <span className="text-muted-foreground">
-                    Glimmer Lamps x <span>2</span>
+                    {selectedOrder?.productName} x <span>1</span>
                   </span>
-                  <span>$250.00</span>
+                  <span>₹{selectedOrder?.price}</span>
                 </li>
-                <li className="flex items-center justify-between">
+                {/* <li className="flex items-center justify-between">
                   <span className="text-muted-foreground">
                     Aqua Filters x <span>1</span>
                   </span>
                   <span>$49.00</span>
-                </li>
+                </li> */}
               </ul>
               <Separator className="my-2" />
               <ul className="grid gap-3">
                 <li className="flex items-center justify-between">
                   <span className="text-muted-foreground">Subtotal</span>
-                  <span>$299.00</span>
+                  <span>₹{selectedOrder?.price}</span>
                 </li>
                 <li className="flex items-center justify-between">
                   <span className="text-muted-foreground">Shipping</span>
-                  <span>$5.00</span>
+                  <span>Free</span>
                 </li>
                 <li className="flex items-center justify-between">
                   <span className="text-muted-foreground">Tax</span>
-                  <span>$25.00</span>
+                  <span>Free</span>
                 </li>
                 <li className="flex items-center justify-between font-semibold">
                   <span className="text-muted-foreground">Total</span>
-                  <span>$329.00</span>
+                  <span>₹{selectedOrder?.price}</span>
                 </li>
               </ul>
             </div>
@@ -317,9 +343,21 @@ export default async function Orders() {
               <div className="grid gap-3">
                 <div className="font-semibold">Shipping Information</div>
                 <address className="grid gap-0.5 not-italic text-muted-foreground">
-                  <span>Liam Johnson</span>
-                  <span>1234 Main St.</span>
-                  <span>Anytown, CA 12345</span>
+                  <span>{selectedOrder?.shippingAddress.fullName}</span>
+                  <span>
+                    {selectedOrder?.shippingAddress.addressLine1}
+                    {selectedOrder?.shippingAddress.addressLine2
+                      ? ` | ${selectedOrder.shippingAddress.addressLine2}`
+                      : ''}
+                    {selectedOrder?.shippingAddress.nearby
+                      ? ` | ${selectedOrder.shippingAddress.nearby}`
+                      : ''}
+                  </span>
+                  <span>
+                    {selectedOrder?.shippingAddress.city},{' '}
+                    {selectedOrder?.shippingAddress.state}{' '}
+                    {selectedOrder?.shippingAddress.postalCode}
+                  </span>
                 </address>
               </div>
               <div className="grid auto-rows-max gap-3">
@@ -335,23 +373,25 @@ export default async function Orders() {
               <dl className="grid gap-3">
                 <div className="flex items-center justify-between">
                   <dt className="text-muted-foreground">Customer</dt>
-                  <dd>Liam Johnson</dd>
+                  <dd>{selectedOrder?.shippingAddress.fullName}</dd>
                 </div>
-                <div className="flex items-center justify-between">
+                {/* <div className="flex items-center justify-between">
                   <dt className="text-muted-foreground">Email</dt>
                   <dd>
                     <a href="mailto:">liam@acme.com</a>
                   </dd>
-                </div>
+                </div> */}
                 <div className="flex items-center justify-between">
                   <dt className="text-muted-foreground">Phone</dt>
                   <dd>
-                    <a href="tel:">+1 234 567 890</a>
+                    <a href={`tel: ${selectedOrder?.shippingAddress.Contact}`}>
+                      {selectedOrder?.shippingAddress.Contact}
+                    </a>
                   </dd>
                 </div>
               </dl>
             </div>
-            <Separator className="my-4" />
+            {/* <Separator className="my-4" />
             <div className="grid gap-3">
               <div className="font-semibold">Payment Information</div>
               <dl className="grid gap-3">
@@ -363,11 +403,13 @@ export default async function Orders() {
                   <dd>**** **** **** 4532</dd>
                 </div>
               </dl>
-            </div>
+            </div> */}
           </CardContent>
           <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
             <div className="text-xs text-muted-foreground">
-              Updated <time dateTime="2023-11-23">November 23, 2023</time>
+              Updated on{' '}
+              {selectedOrder &&
+                format(selectedOrder?.orderDate.toDate(), 'PPP')}
             </div>
             <Pagination className="ml-auto mr-0 w-auto">
               <PaginationContent>
